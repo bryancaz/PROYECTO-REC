b09dc43bccc9d7c76cf9161a759b9d81d17bcca5
bryancaz <bcalzadar@up.edu.mx>
2022-06-15 12:46:14 -0500

    -Se añadio un ejemplo de findIndex

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //findIndex
 //El método findIndex() devuelve el índice del primer elemento de un array que cumpla con la función de prueba proporcionada. En caso contrario devuelve -1.
+//EJEMPLO
+
+const arrayone = [10, 120, 10, 15, 15];
+
+const numerolargo = (element) => element > 15;
+
+console.log(array1.findIndex(numerolargo));
+
+////////////////////////////////////////////////////
