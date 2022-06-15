856cf1d0a9aab41404ba47daab96c78ffac5bd62
bryancaz <bcalzadar@up.edu.mx>
2022-06-15 12:49:31 -0500

    -Se añadio la rama con nombre hoisting

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-//REPLACE
-//El tipo de array en JavaScript nos proporciona el método splice() que nos ayuda a reemplazar los elementos de un array existente eliminando e insertando nuevos elementos en el índice requerido/deseado.
-//EJEMPLO
-
-const texto = 'Habia una vez un perro que solia jugar mucho';
-
-console.log(texto.replace('perro', 'caballo'));
-
-const animal = /perro/i;
-console.log(texto.replace(animal, 'rocky'));
-
-////////////////////////////////////////////////////
+//HOISTING
+//Las declaraciones de funciones antes de ejecutar cualquier otro segmento de código es que permite utilizar una función antes de declararla en el código.
