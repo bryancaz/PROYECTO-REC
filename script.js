04af7ad9b6463004f93551237407d57d99219dc2
bryancaz <bcalzadar@up.edu.mx>
2022-06-15 12:50:14 -0500

    -Se añadio un ejemplo de hoisting

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //HOISTING
 //Las declaraciones de funciones antes de ejecutar cualquier otro segmento de código es que permite utilizar una función antes de declararla en el código.
+//EJEMPLO 
+
+function fullname(fullName) {
+    console.log("Hello " + fullName);
+}
+
+(function() {
+    nombre("Bryan Calzada");
+
+    function nombre(fullName) {
+        console.log("Hola " + fullName + ", Maestro, se me fue la luz todo el fin de semana se que vera esto y le queriacomentar que por eso hasta ahorita pude subir mis array");
+    }
+})();
