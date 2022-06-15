f3d5a0f38e632a35b903c074d5f83fad1acbb22b
bryancaz <bcalzadar@up.edu.mx>
2022-06-15 12:48:34 -0500

    -Se añadio un ejemplo de replace

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //REPLACE
 //El tipo de array en JavaScript nos proporciona el método splice() que nos ayuda a reemplazar los elementos de un array existente eliminando e insertando nuevos elementos en el índice requerido/deseado.
+//EJEMPLO
+
+const texto = 'Habia una vez un perro que solia jugar mucho';
+
+console.log(texto.replace('perro', 'caballo'));
+
+const animal = /perro/i;
+console.log(texto.replace(animal, 'rocky'));
+
+////////////////////////////////////////////////////
